a6032e841bcd7fa58b5210a41ddcdbf78d7c1eba
Ricardo Pelcastre <pelka.dev@gmail.com>
2024-04-19 00:25:32 -0600

    Updated 02

--- 03 - ERCOT Real Time Prices/main.py
+++ 03 - ERCOT Real Time Prices/main.py
@@ -5,6 +5,9 @@ from datetime import datetime, timedelta
 from selectolax.parser import HTMLParser
 
 
+DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+
 def get_data(url: str):
     """
     Retrieves HTML content from the given URL using GET request.
@@ -15,26 +18,6 @@ def get_data(url: str):
     Returns:
         HTMLParser: Parsed HTML content.
     """
-
-    # headers = {
-    #     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
-    #     "accept-language": "en-US,en;q=0.5",
-    #     "cache-control": "max-age=0",
-    #     "cookie": "visid_incap_1217363=40YplimcQJeeAN8TPac9sSr5H2YAAAAAQUIPAAAAAACHlq0JfMNpNLkcEXNtje7K; incap_ses_1605_1217363=ZHt7C5zUpEzJF0S6AxtGFir5H2YAAAAA8MZKhCy+M5FtdUAq/+5KKA==; JSESSIONID=97945DC485C4D1A60EFE7C240A14CCBB.w_prblwbc0093b",
-    #     "if-modified-since": "Wed, 17 Apr 2024 16:32:36 GMT",
-    #     "if-none-match": "97e4-6164d66fd93e7",
-    #     "referer": "https://www.ercot.com/content/cdr/html/20240412_real_time_spp.html",
-    #     "sec-ch-ua": '"Brave";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
-    #     "sec-ch-ua-mobile": "?0",
-    #     "sec-ch-ua-platform": '"Linux"',
-    #     "sec-fetch-dest": "document",
-    #     "sec-fetch-mode": "navigate",
-    #     "sec-fetch-site": "same-origin",
-    #     "sec-fetch-user": "?1",
-    #     "sec-gpc": "1",
-    #     "upgrade-insecure-requests": "1",
-    #     "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
-    # }
 
     res = requests.request("GET", url)
     return HTMLParser(res.text)
@@ -61,6 +44,23 @@ def parse_data(parser: HTMLParser):
     return data
 
 
+def _create_ts(row: pd.Series):
+    """
+    Creates a timestamp from the given row.
+
+    Args:
+        row (pd.Series): A row of the DataFrame.
+
+    Returns:
+        str: The formatted timestamp.
+    """
+    date_parts = row["Oper Day"].split("/")
+    year, month, day = map(int, [date_parts[2], date_parts[0], date_parts[1]])
+    hour, minute = int(row["Interval Ending"][:2]), int(row["Interval Ending"][2:])
+    hour = 0 if hour > 23 else hour
+    return datetime(year, month, day, hour, minute).strftime(DATE_FORMAT)
+
+
 def format_data(data: list[list]):
     """
     Formats parsed data into a DataFrame.
@@ -71,20 +71,10 @@ def format_data(data: list[list]):
     Returns:
         pd.DataFrame: Formatted DataFrame.
     """
-
-    def create_ts(row: any):
-        splited_date = list(map(int, row["Oper Day"].split("/")))
-        hour = int(row["Interval Ending"][:2])
-        hour = 0 if hour > 23 else hour
-        minute = int(row["Interval Ending"][2:])
-        second = 0
-        return datetime(
-            splited_date[2], splited_date[0], splited_date[1], hour, minute, second
-        ).strftime("%Y-%m-%d %H:%M:%S")
-
     df = pd.DataFrame(data[1:], columns=data[0])
-    df["ts"] = df.apply(create_ts, axis=1)
-    df["Creation_Date"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    df["ts"] = df.apply(_create_ts, axis=1)
+    df["Creation_Date"] = datetime.now().strftime(DATE_FORMAT)
+    df["Update_Date"] = None
     return df
 
 
@@ -96,21 +86,21 @@ def save_data(df: pd.DataFrame):
         df (pd.DataFrame): DataFrame to be saved.
     """
     current_path = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(current_path, "result.csv")
 
-    if not os.path.exists(current_path + "/result.csv"):
-        df["Update_Date"] = None
-        df.to_csv(current_path + "/result.csv", index=False)
-    else:
-        old_df = pd.read_csv(f"{current_path}/result.csv")
+    if os.path.exists(file_path):
+        old_df = pd.read_csv(file_path)
         old_df = old_df.drop(columns=["Update_Date"])
         new_df = pd.concat([old_df, df]).drop_duplicates(
-            subset=["Oper Day", "Interval Ending"]
+            subset=["Oper Day", "Interval Ending"], keep="last"
         )
-        new_df["Update_Date"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        new_df.to_csv(current_path + "/result.csv", index=False)
+        new_df["Update_Date"] = datetime.now().strftime(DATE_FORMAT)
+    else:
+        new_df = df.copy()
+    new_df.to_csv(file_path, index=False)
 
 
-def start_taks(past_days: int):
+def start_taks(past_days: int = 1):
     """
     Initiates the data retrieval and saving process.
 
@@ -129,4 +119,4 @@ def start_taks(past_days: int):
 
 
 if __name__ == "__main__":
-    start_taks(3)
+    start_taks(2)
